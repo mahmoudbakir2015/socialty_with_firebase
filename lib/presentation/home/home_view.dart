82d4a8d7573acf00ef0e490f0acc6cd94327d869
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-01-22 18:10:30 +0200

    refactor code

--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:socialty_with_firebase/presentation/home/make_post/make_post.dart';
+import 'package:socialty_with_firebase/presentation/home/items.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isTap = false;
+  @override
   Widget build(BuildContext context) {
     return ListView(
+      physics: const BouncingScrollPhysics(),
       children: [
         buildMakePost(
           context: context,
@@ -15,198 +22,39 @@ class Home extends StatelessWidget {
         const SizedBox(
           height: 10,
         ),
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const Icon(Icons.close),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    const Icon(Icons.list),
-                    const Spacer(),
-                    buildOwnerPost(
-                      name: 'MahmoudBakir',
-                      time: '12 hours',
-                      imgPic: '',
-                      status: false,
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8.0,
-                  ),
-                  child: buildPostContent(
-                    text: null,
-                    image: null,
-                  ),
-                ),
-                buildNumOfLike(
-                  like: '20',
-                  comment: '40',
-                  share: '5',
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    buildPostButton(
-                      name: 'مشاركة',
-                      icon: Icons.share,
-                      onTap: () {},
-                    ),
-                    buildPostButton(
-                      name: 'تعليق',
-                      icon: Icons.comment,
-                      onTap: () {},
-                    ),
-                    buildPostButton(
-                      name: 'اعجبني',
-                      icon: Icons.favorite,
-                      onTap: () {},
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
+        ListView.separated(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemBuilder: (ctx, index) {
+            return buildPostInfo(
+              postWriter: 'MahmoudBakir',
+              time: '10 sec',
+              imgPic: '',
+              isOnline: true,
+              isTap: isTap,
+              postText: null,
+              postImg: null,
+              numLike: '20',
+              numComment: '30',
+              numShare: '10',
+              close: () {},
+              option: () {},
+              onLike: () {
+                setState(() {});
+                isTap = !isTap;
+              },
+              onComment: () {},
+              onShare: () {},
+            );
+          },
+          separatorBuilder: (ctx, index) {
+            return const SizedBox(
+              height: 5,
+            );
+          },
+          itemCount: 10,
         ),
       ],
-    );
-  }
-
-  InkWell buildPostButton({
-    required String name,
-    required IconData icon,
-    required Function()? onTap,
-  }) {
-    return InkWell(
-      onTap: onTap,
-      child: Row(
-        children: [
-          Text(
-            name,
-          ),
-          const SizedBox(
-            width: 3,
-          ),
-          Icon(
-            icon,
-            color: Colors.grey,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Row buildNumOfLike({
-    required String like,
-    required String comment,
-    required String share,
-  }) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          textDirection: TextDirection.rtl,
-          '$comment تعليقا' ' ' '$share مشاركة',
-        ),
-        Text('🌝 $like')
-      ],
-    );
-  }
-
-  Column buildPostContent({
-    String? text,
-    String? image,
-  }) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        (text != null) ? Text(text) : const Text(''),
-        (image != null)
-            ? Image(
-                image: NetworkImage(image),
-              )
-            : const Text(''),
-      ],
-    );
-  }
-
-  Row buildOwnerPost({
-    required String name,
-    required String time,
-    required String imgPic,
-    required bool status,
-  }) {
-    return Row(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              name,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              time,
-              style: const TextStyle(color: Colors.grey),
-            ),
-          ],
-        ),
-        const SizedBox(
-          width: 5,
-        ),
-        Stack(
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(imgPic),
-            ),
-            if (status == true)
-              const Positioned(
-                bottom: 0,
-                left: 0,
-                child: CircleAvatar(
-                  radius: 7,
-                  backgroundColor: Colors.green,
-                ),
-              ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  InkWell buildMakePost({
-    required BuildContext context,
-    required String imgPic,
-  }) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const MakePost(),
-          ),
-        );
-      },
-      child: Container(
-        color: Colors.red,
-        child: ListTile(
-          leading: const Icon(Icons.photo),
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [Text('بم تفكر ؟')],
-          ),
-          trailing: CircleAvatar(
-            backgroundImage: NetworkImage(imgPic),
-          ),
-        ),
-      ),
     );
   }
 }
---
+++ lib/presentation/home/items.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:socialty_with_firebase/presentation/home/make_post/make_post.dart';
+
+Card buildPostInfo({
+  required String postWriter,
+  required String time,
+  required String imgPic,
+  required bool isOnline,
+  bool isTap = false,
+  required Function()? close,
+  required Function()? option,
+  required Function()? onLike,
+  required Function()? onComment,
+  required Function()? onShare,
+  String? postText,
+  String? postImg,
+  String numLike = '0',
+  String numComment = '0',
+  String numShare = '0',
+}) {
+  return Card(
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              GestureDetector(
+                onTap: close,
+                child: const Icon(
+                  Icons.close,
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              GestureDetector(
+                onTap: option,
+                child: const Icon(
+                  Icons.list,
+                ),
+              ),
+              const Spacer(),
+              buildOwnerPost(
+                name: postWriter,
+                time: time,
+                imgPic: imgPic,
+                status: isOnline,
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8.0,
+            ),
+            child: buildPostContent(
+              text: postText,
+              image: postImg,
+            ),
+          ),
+          buildNumOfLike(
+            like: numLike,
+            comment: numComment,
+            share: numShare,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                buildPostButton(
+                  name: 'مشاركة',
+                  icon: Icons.share,
+                  onTap: onShare,
+                ),
+                buildPostButton(
+                  name: 'تعليق',
+                  icon: Icons.comment,
+                  onTap: onComment,
+                ),
+                buildPostButton(
+                  isTap: isTap,
+                  name: 'اعجبني',
+                  icon: Icons.favorite,
+                  onTap: onLike,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+InkWell buildPostButton({
+  required String name,
+  required IconData icon,
+  required Function()? onTap,
+  bool isTap = false,
+}) {
+  return InkWell(
+    onTap: onTap,
+    child: Row(
+      children: [
+        Text(
+          name,
+        ),
+        const SizedBox(
+          width: 3,
+        ),
+        Icon(
+          icon,
+          color: isTap ? Colors.red : Colors.grey,
+        ),
+      ],
+    ),
+  );
+}
+
+Row buildNumOfLike({
+  required String like,
+  required String comment,
+  required String share,
+}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Text(
+        textDirection: TextDirection.rtl,
+        '$comment تعليقا' ' ' '$share مشاركة',
+      ),
+      Text('🌝 $like')
+    ],
+  );
+}
+
+Column buildPostContent({
+  String? text,
+  String? image,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      (text != null) ? Text(text) : const Text(''),
+      (image != null)
+          ? Image(
+              image: NetworkImage(image),
+            )
+          : const Text(''),
+    ],
+  );
+}
+
+Row buildOwnerPost({
+  required String name,
+  required String time,
+  required String imgPic,
+  required bool status,
+}) {
+  return Row(
+    children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            name,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            time,
+            style: const TextStyle(color: Colors.grey),
+          ),
+        ],
+      ),
+      const SizedBox(
+        width: 5,
+      ),
+      Stack(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(imgPic),
+          ),
+          if (status == true)
+            const Positioned(
+              bottom: 0,
+              left: 0,
+              child: CircleAvatar(
+                radius: 7,
+                backgroundColor: Colors.green,
+              ),
+            ),
+        ],
+      ),
+    ],
+  );
+}
+
+InkWell buildMakePost({
+  required BuildContext context,
+  required String imgPic,
+}) {
+  return InkWell(
+    onTap: () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const MakePost(),
+        ),
+      );
+    },
+    child: Container(
+      color: Colors.red,
+      child: ListTile(
+        leading: const Icon(Icons.photo),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [Text('بم تفكر ؟')],
+        ),
+        trailing: CircleAvatar(
+          backgroundImage: NetworkImage(imgPic),
+        ),
+      ),
+    ),
+  );
+}
