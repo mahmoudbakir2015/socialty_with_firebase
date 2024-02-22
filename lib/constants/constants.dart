2b78cb7a4d86c122dd22384d4c9ae56c427b836f
mahmoudbakir2015 <76684888+mahmoudbakir2015@users.noreply.github.com>
2024-02-22 06:05:18 +0200

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,6 +2,4 @@ abstract class Constants {
   static const double appPadding = 20;
   static const String imgTest = 'https://wallpapercave.com/wp/wp2568544.jpg';
   static const String notifUrl = 'https://fcm.googleapis.com/fcm/send';
-  static const String serverToken =
-      'AAAAiHaixbw:APA91bGWaK38EpPulZEmfkcl1kz0thFV-EUjDEALBDgBVe4bRc2bRqvu8aRupEYDFnMWdc2fwcOMYBDlAopMzV2t0sfTUKotVOaBV1rpm3lLkd6GlP66gzKP11vm5TjReHZtJeBcJcLI';
 }
